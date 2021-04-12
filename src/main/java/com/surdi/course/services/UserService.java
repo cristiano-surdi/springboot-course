240cebbf73884e7e1aa7b11b9880953bc172b4f7
Cristiano Surdi <cristianosurdi@gmail.com>
2021-04-12 18:53:39 -0300

    Service layer, component registration

---
+++ src/main/java/com/surdi/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.surdi.course.services;
+
+import com.surdi.course.entities.User;
+import com.surdi.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
