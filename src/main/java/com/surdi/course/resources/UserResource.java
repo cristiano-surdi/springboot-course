fc3a3d58f530faea003c554e04152943ade960f7
Cristiano Surdi <cristianosurdi@gmail.com>
2021-04-20 14:31:36 -0300

    cors

--- src/main/java/com/surdi/course/resources/UserResource.java
+++ src/main/java/com/surdi/course/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
