0c7ee288683f585a3ed5cfa14f9c3dec1550f93b
Cristiano Surdi <cristianosurdi@gmail.com>
2021-04-23 23:49:57 -0300

    Category Resource, Product Resource

--- src/main/java/com/surdi/course/services/CategoryService.java
+++ src/main/java/com/surdi/course/services/CategoryService.java
@@ -2,9 +2,14 @@ package com.surdi.course.services;
 
 import com.surdi.course.entities.Category;
 import com.surdi.course.repositories.CategoryRepository;
+import com.surdi.course.services.exceptions.DatabaseException;
+import com.surdi.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,5 +26,33 @@ public class CategoryService {
     public Category findById(Long id) {
         Optional<Category> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public Category insert(Category obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Category update(Long id, Category obj) {
+        try {
+            Category entity = repository.getOne(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Category entity, Category obj) {
+        entity.setName(obj.getName());
     }
 }
--- src/main/java/com/surdi/course/services/ProductService.java
+++ src/main/java/com/surdi/course/services/ProductService.java
@@ -2,9 +2,14 @@ package com.surdi.course.services;
 
 import com.surdi.course.entities.Product;
 import com.surdi.course.repositories.ProductRepository;
+import com.surdi.course.services.exceptions.DatabaseException;
+import com.surdi.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,5 +26,36 @@ public class ProductService {
     public Product findById(Long id) {
         Optional<Product> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public Product insert(Product obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Product update(Long id, Product obj) {
+        try {
+            Product entity = repository.getOne(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Product entity, Product obj) {
+        entity.setName(obj.getName());
+        entity.setDescription(obj.getDescription());
+        entity.setPrice(obj.getPrice());
+        entity.setImgUrl(obj.getImgUrl());
     }
 }
